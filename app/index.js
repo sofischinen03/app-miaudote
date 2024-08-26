0108769aa40e98f98ee01baf19cbb8a594fbd8ff
sofischinen03 <sofia.costa11@etec.sp.gov.br>
2024-08-26 09:46:13 -0300

    mensagem

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { Text, View, Pressable, StyleSheet, Image, ScrollView } from "react-native";
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>App de Adoção</Text>
+
+      <Link href="/"
+    </View>
+  );
+}
